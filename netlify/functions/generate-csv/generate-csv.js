3c26d9a7787cbfe69b3bda3c874a15b2480bc0c8
rudy <user@Mac.lan>
2025-04-25 14:49:31 +0200

    Fix: Use 'mailgun-js' for email sending in generate-csv

--- netlify/functions/generate-csv/generate-csv.js
+++ netlify/functions/generate-csv/generate-csv.js
@@ -1,12 +1,12 @@
 const parser = require('lambda-multipart-parser');
-const sgMail = require('@sendgrid/mail');
+const formData = require('form-data');
+const Mailgun = require('mailgun-js');
+const mailgun = Mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
 
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Utiliser une variable d'environnement pour la clé API
 
   try {
     const result = await parser.parse(event);
@@ -22,8 +22,15 @@ exports.handler = async (event) => {
     const email = result.email;
 
     let sponsorLogoFilename = '';
-    if (sponsorLogoFile && sponsorLogoFile.filename) {
+    let sponsorLogoAttachment = null;
+
+    if (sponsorLogoFile && sponsorLogoFile.filename && sponsorLogoFile.content) {
       sponsorLogoFilename = sponsorLogoFile.filename;
+      sponsorLogoAttachment = new mailgun.Attachment({
+        data: Buffer.from(sponsorLogoFile.content, 'base64'),
+        filename: sponsorLogoFilename,
+        contentType: sponsorLogoFile.contentType,
+      });
     }
 
     let csvString = "Nom de l'équipe,Maillot,Nom,Taille,Numéro,Anecdote,Logo Sponsor,Email Capitaine\n";
@@ -38,28 +45,26 @@ exports.handler = async (event) => {
     }
 
     const filename = `inscription_${teamName?.replace(/\s+/g, '_')}.csv`;
-    const base64CSV = Buffer.from(csvString).toString('base64');
+    const csvAttachment = new mailgun.Attachment({ data: Buffer.from(csvString), filename: filename, contentType: 'text/csv' });
 
-    const msg = {
-      to: 'rudy.masse@gmail.com', // Ton adresse email
-      from: email, // L'email du capitaine comme expéditeur (peut nécessiter une configuration spécifique chez SendGrid)
+    const attachments = [csvAttachment];
+    if (sponsorLogoAttachment) {
+      attachments.push(sponsorLogoAttachment);
+    }
+
+    const data = {
+      from: `Inscriptions Beach Rugby <postmaster@${process.env.MAILGUN_DOMAIN}>`, // Adresse d'envoi
+      to: `rudy masse <rudy.masse@gmail.com>`, // Ton adresse de réception
       subject: `Nouvelle inscription pour l'équipe ${teamName}`,
       text: `Ci-joint le fichier CSV des inscriptions de l'équipe ${teamName}.`,
-      attachments: [
-        {
-          content: base64CSV,
-          filename: filename,
-          type: 'text/csv',
-          disposition: 'attachment',
-        },
-      ],
+      attachment: attachments,
     };
 
-    await sgMail.send(msg);
+    await mailgun.messages().send(data);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Inscription enregistrée pour ${teamName}. Un email avec les données CSV a été envoyé à rudy.masse@gmail.com` }),
+      body: JSON.stringify({ message: `Inscription enregistrée pour ${teamName}. Un email avec les données a été envoyé.` }),
     };
 
   } catch (error) {
